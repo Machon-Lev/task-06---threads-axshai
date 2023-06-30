227773d849badbd7d0027da0b8163ebac21f81e5
Shai Axelrod <Saxelrod@g.jct.ac.il>
2023-06-30 12:00:27 +0300

    add doc to message class

--- task8_Shai_Axelrod/Threads/src/Message.cpp
+++ task8_Shai_Axelrod/Threads/src/Message.cpp
@@ -2,6 +2,7 @@
 
 std::ostream& operator<<(std::ostream& os, const Message& msg)
 {
+	// Print in the appropriate format
 	if (msg._isLast) {
 		os << msg._threadId << " finished" << std::endl;
 	}
@@ -16,7 +17,13 @@ void Message::write(std::thread::id threadId, double data, bool isLast)
 	_threadId = threadId;
 	_data = data;
 	_isLast = isLast;
+	// this is a new message - so set _isRead to false
 	_isRead = false;
+}
+
+bool Message::wasLast()
+{ 
+	return _isLast; 
 }
 
 bool Message::isRead()
